46dc49a86cb3eb8fbc18e336e6800e1167bc0da5
ezatechbd <enamulhaque556315@gmail.com>
2022-01-06 11:26:52 +0600

    alarm manager scheduler work anywhere

---
+++ flutter_scheduler/work anywhere/flutter_scheduler/lib/fcm_utils.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  'This channel is used for important notifications.', // description
+  importance: Importance.high,
+  playSound: true,
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void fireAlarm() {
+  flutterLocalNotificationsPlugin.show(
+    0,
+    "Testing notification",
+    "Well! Received notification Successfully",
+    NotificationDetails(
+      android: AndroidNotificationDetails(
+        channel.id,
+        channel.name,
+        channel.description,
+        importance: Importance.high,
+        color: Colors.blue,
+        playSound: true,
+        icon: '@mipmap/ic_launcher',
+      ),
+    ),
+  );
+  print(':::::::::::::::::::::object:::::::::::::::::::::');
+}
